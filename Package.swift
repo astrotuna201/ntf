7b1bfb58958a46b84a25dc11d2024fd7a5eb8cda
Edward Connell <ewconnell@gmail.com>
2019-03-11 09:58:46 -0700

    adding komondor

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"),
+        .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"),
+        .package(url: "https://github.com/orta/Komondor", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,3 +30,21 @@ let package = Package(
             dependencies: ["Netlib"]),
     ]
 )
+
+// The settings for the git hooks for our repo
+#if canImport(PackageConfig)
+import PackageConfig
+
+let config = PackageConfig([
+    "komondor": [
+        // When someone has run `git commit`, first run
+        // run SwiftFormat and the auto-correcter for SwiftLint
+        "pre-commit": [
+            "swift run swiftformat .",
+            "swift run swiftlint autocorrect --path Netlib/",
+            "git add .",
+        ],
+    ]
+    ])
+#endif
+

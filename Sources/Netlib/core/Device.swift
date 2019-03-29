6007d692a0a1a87b42f5a10fdf1ec3f377934893
Edward Connell <ewconnell@gmail.com>
2019-03-28 19:15:40 -0700

    snap

--- Sources/Netlib/core/Device.swift
+++ Sources/Netlib/core/Device.swift
@@ -90,9 +90,9 @@ public protocol StreamEvent: ObjectTracking, Logging {
 public struct StreamEventOptions: OptionSet {
     public init(rawValue: Int) { self.rawValue = rawValue }
     public let rawValue: Int
-    public static let hostSync     = StreamEventOptions(rawValue: 1 << 0)
-    public static let timing       = StreamEventOptions(rawValue: 1 << 1)
-    public static let interProcess = StreamEventOptions(rawValue: 1 << 2)
+    /// hostSync     = StreamEventOptions(rawValue: 1 << 0)
+    /// timing       = StreamEventOptions(rawValue: 1 << 1)
+    /// interProcess = StreamEventOptions(rawValue: 1 << 2)
 }
 
 //==============================================================================
@@ -100,6 +100,8 @@ public struct StreamEventOptions: OptionSet {
 /// A device stream is an asynchronous queue of commands executed on
 /// the associated device
 public protocol DeviceStream: ObjectTracking, Logging {
+    //--------------------------------------------------------------------------
+    // properties
     /// the device the stream is associated with
     var device: ComputeDevice { get }
     /// a unique id used to identify the stream
@@ -107,34 +109,8 @@ public protocol DeviceStream: ObjectTracking, Logging {
     /// a name used to identify the stream
     var name: String { get }
 
-    //-------------------------------------
-    /// execute function
-    /// - Parameter functionId: id of the function to execute
-    /// - Parameter instanceId: function instance id for associated resources
-    /// - Parameter parameters: parameters structure to serialize
-    ///
-    func execute<T>(functionId: UUID, with parameters: T) throws
-    
-    //-------------------------------------
-    /// setup a function instance and assoicated resources for
-    /// repetative execution (e.g. convolution). The instanceId can
-    /// susequently be used with the `execute(functionId:` function
-    ///
-    /// - Parameter functionId: The function class to create
-    /// - Parameter instanceId: The function instance id
-    /// - Parameter parameters: The configuration parameters
-    ///
-    func setup<T>(functionId: UUID, instanceId: UUID, with parameters: T) throws
-
-    //-------------------------------------
-    /// release function instance created by createFunctionInstance
-    ///
-    /// - Parameter instance: The id of the function instance to release
-    ///
-    func release(instanceId: UUID) throws
-    
-    //-------------------------------------
-    // synchronization
+    //--------------------------------------------------------------------------
+    // synchronization functions
     /// blocks the calling thread until the stream queue is empty
     func blockCallerUntilComplete() throws
     /// creates a StreamEvent
@@ -148,4 +124,188 @@ public protocol DeviceStream: ObjectTracking, Logging {
     func sync(with other: DeviceStream, event: StreamEvent) throws
     /// blocks caller until the event has occurred
     func wait(for event: StreamEvent) throws
+
+    //--------------------------------------------------------------------------
+    // intrinsic functions
+    /// Computes the absolute value of the specified TensorView element-wise.
+    func abs<T: SignedNumeric>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Adds two tensors and produces their sum.
+    /// - Note: `+` supports broadcasting.
+    func add<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                         result: inout TensorView<T>) throws
+
+    /// Returns `true` if all scalars are equal to `true`. Otherwise, returns
+    /// `false`.
+    func all(x: TensorView<Bool>, result: inout TensorView<Bool>) throws
+
+    /// Returns a `true` scalar if any scalars are equal to `true`.
+    /// Otherwise returns a `false` scalar
+    func any(x: TensorView<Bool>, result: inout TensorView<Bool>) throws
+
+    /// Performs a pointwise comparison within the specified tolerance
+    func approximatelyEqual<T: FloatingPoint>(
+        lhs: TensorView<T>, rhs: TensorView<T>,
+        tolerance: Double,
+        result: inout TensorView<Bool>) throws
+    
+    /// Returns the indices of the maximum values along the specified axes. The
+    /// reduced dimensions are removed.
+    /// - Parameter axes: The dimensions to reduce.
+    /// - Precondition: Each value in `axes` must be in the range `-rank..<rank`.
+    func argmax<T: Numeric>(x: TensorView<T>, squeezingAxis axis: Int,
+                            result: inout TensorView<Int32>) throws
+
+    /// Returns the indices of the minimum values along the specified axes. The
+    /// reduced dimensions are removed.
+    /// - Parameter axes: The dimensions to reduce.
+    /// - Precondition: Each value in `axes` must be in the range `-rank..<rank`.
+    func argmin<T: Numeric>(x: TensorView<T>, squeezingAxis axis: Int,
+                            result: inout TensorView<Int32>) throws
+
+    /// Broadcast x to the specified shape
+    /// - Parameter x: the pattern to broadcast
+    /// - Parameter shape: the shape of the result
+    /// - Precondition: The specified shape must be compatible for broadcasting.
+    func broadcast<T>(x: TensorView<T>, toShape shape: DataShape,
+                      result: inout TensorView<T>) throws
+    /// cast scalar types
+    /// - Parameter from: the input data
+    /// - Parameter result: the output
+    func cast<T,U>(from: TensorView<T>, to result: inout TensorView<U>) throws
+    
+    /// Computes the ceiling of the specified TensorView element-wise.
+    func ceil<T : FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Concatenates tensors along the specified axis.
+    /// - Precondition: The tensors must have the same dimensions, except for the
+    ///                 specified axis.
+    /// - Precondition: The axis must be in the range `-rank..<rank`.
+    func concatenate<T>(view: TensorView<T>, with other: TensorView<T>,
+                        alongAxis axis: Int, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `cos`
+    func cos<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `cosh`
+    func cohs<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Returns the quotient of dividing the first TensorView by the second.
+    /// - Note: `/` supports broadcasting.
+    func div<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                         result: inout TensorView<T>) throws
+    
+    /// Computes `lhs == rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    /// - Note: `.==` supports broadcasting.
+    func equal<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                           result: inout TensorView<Bool>) throws
+
+    /// Computes the element-wise `exp`
+    func exp<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Computes the element-wise `floor`
+    func floor<T : FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Computes `lhs > rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    func greater<T: Numeric & Comparable>(
+        lhs: TensorView<T>, rhs: TensorView<T>,
+        result: inout TensorView<Bool>) throws
+
+    /// Computes `lhs >= rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    func greaterOrEqual<T: Numeric & Comparable>(
+        lhs: TensorView<T>, rhs: TensorView<T>,
+        result: inout TensorView<Bool>) throws
+
+    /// Computes `lhs < rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    func less<T: Numeric & Comparable>(
+        lhs: TensorView<T>, rhs: TensorView<T>,
+        result: inout TensorView<Bool>) throws
+    
+    /// lessEqual
+    /// Computes `lhs <= rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    func lessOrEqual<T: Numeric & Comparable>(
+        lhs: TensorView<T>, rhs: TensorView<T>,
+        result: inout TensorView<Bool>) throws
+
+    /// Computes the element-wise `log`
+    func log<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `!x`
+    func logicalNot(x: TensorView<Bool>, result: inout TensorView<Bool>) throws
+    
+    /// Computes the element-wise `lhs && rhs`
+    func logicalAnd(lhs: TensorView<Bool>, rhs: TensorView<Bool>,
+                    result: inout TensorView<Bool>) throws
+    
+    /// Computes the element-wise `lhs || rhs`
+    func logicalOr(lhs: TensorView<Bool>, rhs: TensorView<Bool>,
+                   result: inout TensorView<Bool>) throws
+
+    /// Computes the element-wise `logSoftmax`
+    func logSoftmax<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Performs matrix multiplication with another TensorView and produces the
+    /// result.
+    func matmul<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                            result: inout TensorView<T>) throws
+
+    /// max
+    /// maximum
+    /// mean
+    /// min
+    /// minimum
+    /// mod
+    /// mul
+    func mul<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                         result: inout TensorView<T>) throws
+    /// neg
+    
+    /// Computes `lhs != rhs` element-wise and returns a `TensorView` of Boolean
+    /// scalars.
+    /// - Note: `.==` supports broadcasting.
+    func notEqual<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                              result: inout TensorView<Bool>) throws
+
+    /// pad
+    /// pow
+    /// prod
+
+    /// Computes the element-wise `rsqrt`
+    func rsqrt<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// select
+    
+    /// Computes the element-wise `sin`
+    func sin<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `sinh`
+    func sinh<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `square`
+    func square<T: Numeric>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Computes the element-wise `(lhs - rhs)**2`
+    func squaredDifference<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                                       result: inout TensorView<T>) throws
+
+    /// Computes the element-wise `sqrt`
+    func sqrt<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// subtract
+    func subtract<T: Numeric>(lhs: TensorView<T>, rhs: TensorView<T>,
+                              result: inout TensorView<T>) throws
+    
+    /// Sums the input to produce a scalar
+    func sum<T: Numeric>(x: TensorView<T>, result: inout TensorView<T>) throws
+
+    /// Computes the element-wise `tan`
+    func tan<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
+    
+    /// Computes the element-wise `tanh`
+    func tanh<T: FloatingPoint>(x: TensorView<T>, result: inout TensorView<T>) throws
 }

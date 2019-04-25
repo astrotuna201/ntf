4893f80803eef2135bcfa1969f734cfe91d4f331
Edward Connell <ewconnell@gmail.com>
2019-04-25 12:13:00 -0700

    removed unneeded test

--- Tests/NetlibTests/XCTestManifests.swift
+++ Tests/NetlibTests/XCTestManifests.swift
@@ -5,7 +5,6 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(test_DataMigration.allTests),
         testCase(test_IterateView.allTests),
-//        testCase(test_StreamOps.allTests),
         testCase(test_Syntax.allTests),
     ]
 }

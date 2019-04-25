6ec2465a73868fc036a10cefdef97174ba25d7ac
Edward Connell <ewconnell@gmail.com>
2019-04-24 17:00:46 -0700

    updated test manifest

--- Tests/NetlibTests/XCTestManifests.swift
+++ Tests/NetlibTests/XCTestManifests.swift
@@ -5,7 +5,8 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(test_DataMigration.allTests),
         testCase(test_IterateView.allTests),
-        testCase(test_StreamOps.allTests),
+//        testCase(test_StreamOps.allTests),
+        testCase(test_Syntax.allTests),
     ]
 }
 #endif

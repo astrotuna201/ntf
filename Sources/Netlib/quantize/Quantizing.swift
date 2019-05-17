19b661967cb9f4fe6353b8cb062aa2ef43245270
Edward Connell <ewconnell@gmail.com>
2019-05-17 08:19:44 -0700

    Quantizing is defined

--- Sources/Netlib/quantize/Quantizing.swift
+++ Sources/Netlib/quantize/Quantizing.swift
@@ -6,10 +6,26 @@ import Foundation
 
 //==============================================================================
 /// Quantizing
-/// performs quantized tensor indexing
+/// enables quantized tensor indexing
 public protocol Quantizing where Self: TensorView {
-    associatedtype Quant: Quantizer where Quant.Element == Self.Element, Quant.Viewed == Self.Values.Element
-    var quantizer: Quant { get }
+    // types
+    associatedtype Viewed
+    
+    // properties
+    /// bias applied to Viewed value
+    var bias: Float { get set }
+    /// scale applied to Viewed value
+    var scale: Float { get set }
+    /// integer to float transform scale
+    var _transformScale: Float { get set }
+    /// float to integer transform scale
+    var _inverseTransformScale: Float { get set }
+    
+    /// converts the tensor element value type to the viewed value type
+    func convert(element: Element) -> Viewed
+    /// converts the tensor viewed value type to the element value type
+    func convert(viewed: Viewed) -> Element
+
 }
 
 //==============================================================================
@@ -23,7 +39,7 @@ public struct QTensorValueCollection<View>: RandomAccessCollection
     public let startIndex: View.Index
     public let endIndex: View.Index
     public let count: Int
-    public let padValue: View.Values.Element
+    public let padValue: View.Viewed
 
     // initializers
     public init(view: View, buffer: UnsafeBufferPointer<View.Element>) throws {
@@ -32,7 +48,7 @@ public struct QTensorValueCollection<View>: RandomAccessCollection
         startIndex = view.startIndex
         endIndex = view.endIndex
         count = view.elementCount
-        padValue = view.quantizer.convert(element: view.padValue)
+        padValue = view.convert(element: view.padValue)
     }
 
     //--------------------------------------------------------------------------
@@ -48,9 +64,9 @@ public struct QTensorValueCollection<View>: RandomAccessCollection
     }
 
     @inlinable @inline(__always)
-    public subscript(index: View.Index) -> View.Values.Element {
+    public subscript(index: View.Index) -> View.Viewed {
         return index.isPad ? padValue :
-            view.quantizer.convert(element: buffer[index.dataIndex])
+            view.convert(element: buffer[index.dataIndex])
     }
 }
 
@@ -65,7 +81,7 @@ public struct QTensorMutableValueCollection<View>: RandomAccessCollection,
     public let startIndex: View.Index
     public let endIndex: View.Index
     public let count: Int
-    public let padValue: View.Values.Element
+    public let padValue: View.Viewed
 
     // initializers
     public init(view: inout View,
@@ -75,7 +91,7 @@ public struct QTensorMutableValueCollection<View>: RandomAccessCollection,
         startIndex = view.startIndex
         endIndex = view.endIndex
         count = view.elementCount
-        padValue = view.quantizer.convert(element: view.padValue)
+        padValue = view.convert(element: view.padValue)
     }
 
     //--------------------------------------------------------------------------
@@ -91,13 +107,13 @@ public struct QTensorMutableValueCollection<View>: RandomAccessCollection,
     }
 
     @inlinable @inline(__always)
-    public subscript(index: View.Index) -> View.Values.Element {
+    public subscript(index: View.Index) -> View.Viewed {
         get {
             return index.isPad ? padValue :
-                view.quantizer.convert(element: buffer[index.dataIndex])
+                view.convert(element: buffer[index.dataIndex])
         }
         set {
-            buffer[index.dataIndex] = view.quantizer.convert(viewed: newValue)
+            buffer[index.dataIndex] = view.convert(viewed: newValue)
         }
     }
 }

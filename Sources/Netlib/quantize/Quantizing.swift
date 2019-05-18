321bf568a5ec255a6fc7ecb3ae3b00c5dcebedca
Edward Connell <ewconnell@gmail.com>
2019-05-18 12:05:05 -0700

    updated QTensorValueCollection to match new Quantizing definition

--- Sources/Netlib/quantize/Quantizing.swift
+++ Sources/Netlib/quantize/Quantizing.swift
@@ -31,92 +31,86 @@ public extension Quantizing {
     }
 }
 
-////==============================================================================
-///// QTensorValueCollection
-//public struct QTensorValueCollection<View>: RandomAccessCollection
-//    where View: TensorView & Quantizing
-//{
-//    // properties
-//    public let view: View
-//    public let buffer: UnsafeBufferPointer<View.Element>
-//    public let startIndex: View.Index
-//    public let endIndex: View.Index
-//    public let count: Int
-//    public let padValue: View.Viewed
-//
-//    // initializers
-//    public init(view: View, buffer: UnsafeBufferPointer<View.Element>) throws {
-//        self.view = view
-//        self.buffer = buffer
-//        startIndex = view.startIndex
-//        endIndex = view.endIndex
-//        count = view.elementCount
-//        padValue = view.convert(element: view.padValue)
-//    }
-//
-//    //--------------------------------------------------------------------------
-//    // Collection
-//    @inlinable @inline(__always)
-//    public func index(before i: View.Index) -> View.Index {
-//        return i.advanced(by: -1)
-//    }
-//
-//    @inlinable @inline(__always)
-//    public func index(after i: View.Index) -> View.Index {
-//        return i.increment()
-//    }
-//
-//    @inlinable @inline(__always)
-//    public subscript(index: View.Index) -> View.Viewed {
-//        return index.isPad ? padValue :
-//            view.convert(element: buffer[index.dataIndex])
-//    }
-//}
-//
-////==============================================================================
-///// QTensorMutableValueCollection
-//public struct QTensorMutableValueCollection<View>: RandomAccessCollection,
-//    MutableCollection where View: TensorView & Quantizing
-//{
-//    // properties
-//    public let view: View
-//    public let buffer: UnsafeMutableBufferPointer<View.Element>
-//    public let startIndex: View.Index
-//    public let endIndex: View.Index
-//    public let count: Int
-//    public let padValue: View.Viewed
-//
-//    // initializers
-//    public init(view: inout View,
-//                buffer: UnsafeMutableBufferPointer<View.Element>) throws {
-//        self.view = view
-//        self.buffer = buffer
-//        startIndex = view.startIndex
-//        endIndex = view.endIndex
-//        count = view.elementCount
-//        padValue = view.convert(element: view.padValue)
-//    }
-//
-//    //--------------------------------------------------------------------------
-//    // Collection
-//    @inlinable @inline(__always)
-//    public func index(before i: View.Index) -> View.Index {
-//        return i.advanced(by: -1)
-//    }
-//
-//    @inlinable @inline(__always)
-//    public func index(after i: View.Index) -> View.Index {
-//        return i.increment()
-//    }
-//
-//    @inlinable @inline(__always)
-//    public subscript(index: View.Index) -> View.Viewed {
-//        get {
-//            return index.isPad ? padValue :
-//                view.convert(element: buffer[index.dataIndex])
-//        }
-//        set {
-//            buffer[index.dataIndex] = view.convert(viewed: newValue)
-//        }
-//    }
-//}
+//==============================================================================
+/// QTensorValueCollection
+public struct QTensorValueCollection<View>: RandomAccessCollection
+    where View: TensorView & Quantizing
+{
+    // properties
+    public let view: View
+    public let buffer: UnsafeBufferPointer<View.Element>
+    public let startIndex: View.Index
+    public let endIndex: View.Index
+    public let count: Int
+
+    // initializers
+    public init(view: View, buffer: UnsafeBufferPointer<View.Element>) throws {
+        self.view = view
+        self.buffer = buffer
+        startIndex = view.startIndex
+        endIndex = view.endIndex
+        count = view.shape.elementCount
+    }
+
+    //--------------------------------------------------------------------------
+    // Collection
+    @inlinable @inline(__always)
+    public func index(before i: View.Index) -> View.Index {
+        return i.advanced(by: -1)
+    }
+
+    @inlinable @inline(__always)
+    public func index(after i: View.Index) -> View.Index {
+        return i.increment()
+    }
+
+    @inlinable @inline(__always)
+    public subscript(index: View.Index) -> View.Viewed {
+        return view.convert(element: buffer[index.dataIndex])
+    }
+}
+
+//==============================================================================
+/// QTensorMutableValueCollection
+public struct QTensorMutableValueCollection<View>: RandomAccessCollection,
+    MutableCollection where View: TensorView & Quantizing
+{
+    // properties
+    public let view: View
+    public let buffer: UnsafeMutableBufferPointer<View.Element>
+    public let startIndex: View.Index
+    public let endIndex: View.Index
+    public let count: Int
+
+    // initializers
+    public init(view: inout View,
+                buffer: UnsafeMutableBufferPointer<View.Element>) throws {
+        self.view = view
+        self.buffer = buffer
+        startIndex = view.startIndex
+        endIndex = view.endIndex
+        count = view.shape.elementCount
+    }
+
+    //--------------------------------------------------------------------------
+    // Collection
+    @inlinable @inline(__always)
+    public func index(before i: View.Index) -> View.Index {
+        return i.advanced(by: -1)
+    }
+
+    @inlinable @inline(__always)
+    public func index(after i: View.Index) -> View.Index {
+        return i.increment()
+    }
+
+    @inlinable @inline(__always)
+    public subscript(index: View.Index) -> View.Viewed {
+        get {
+            return view.convert(element: buffer[index.dataIndex])
+        }
+        set {
+            buffer[index.dataIndex] = view.convert(viewed: newValue)
+        }
+    }
+}

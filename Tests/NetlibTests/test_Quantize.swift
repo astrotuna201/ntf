8505c316862a83d48df616606dec4f33df564b06
Edward Connell <ewconnell@gmail.com>
2019-05-21 11:55:42 -0700

    added test_Quantize unit tests

---
+++ Tests/NetlibTests/test_Quantize.swift
@@ -0,0 +1,48 @@
+//******************************************************************************
+//  Created by Edward Connell on 5/21/19
+//  Copyright © 2019 Connell Research. All rights reserved.
+//
+import XCTest
+import Foundation
+
+@testable import Netlib
+
+class test_Quantize: XCTestCase {
+    //==========================================================================
+    // support terminal test run
+    static var allTests = [
+        ("test_moment", test_moment),
+        ("test_UInt8Float", test_UInt8Float),
+        ("test_FloatUInt8", test_FloatUInt8),
+    ]
+    
+    //==========================================================================
+    // test_moment
+    //
+    func test_moment() {
+        //        do {
+        //        } catch {
+        //            XCTFail(String(describing: error))
+        //        }
+    }
+
+    //==========================================================================
+    // test_UInt8Float
+    //
+    func test_UInt8Float() {
+//        do {
+//        } catch {
+//            XCTFail(String(describing: error))
+//        }
+    }
+
+    //==========================================================================
+    // test_FloatUInt8
+    //
+    func test_FloatUInt8() {
+        //        do {
+        //        } catch {
+        //            XCTFail(String(describing: error))
+        //        }
+    }
+}
